7bac1747c5fcc0cffa00290958c3556e09f88795
Valirou <68293328+Valirou@users.noreply.github.com>
2020-08-14 17:33:31 -0400

    I started to code the loop()
    1) I changed the PID library because James gave me a new version
    2) I wrote the begging of loop() and added/modified multiple details in the code

--- _main/_main.ino
+++ _main/_main.ino
@@ -16,7 +16,7 @@
  *  This fonction does not have any entries or returns
  *   
  *   Calling:                       
- *   obtainData();
+ *   _main();
 ---------------------------------------------------------------------------------------------------*/
 
 /*---------------------------------------------------------------------------------------------------
@@ -47,7 +47,7 @@
  *  **************************************************************************************************
  *  Proximity sensor
  *  from https://abra-electronics.com/sensors/sensors-proximity-en/466-vcnl4000-proximity-light-sensor.html
- *  Address (I2C) :
+ *  Address (I2C) : 0x13
  *  
  *  VCNL4010  → Arduino MEGA or intermediate bus
  *  VIN       → 5V bus
@@ -56,15 +56,15 @@
  *  SDA       → SDA 20 bus
  *  **************************************************************************************************
  *  Solid state relay (SSR)
- *  from ...
+ *  from http://www.crydom.com/en/products/panel-mount/perfect-fit/ac-output/series-1/d2450/
  *  Address (I2C) : none
  *  
- *  ...               → Arduino MEGA or intermediate bus
- *  ...               → PWM 2
- *  GND               → GND bus
+ *  D2450           → Arduino MEGA or intermediate bus
+ *  3               → PWM 2
+ *  4               → GND bus
  *  In AC circuit :
- *  ...               → heating switch (120 VAC supply)
- *  ...               → heating element
+ *  1               → heating switch (120 VAC supply)
+ *  2               → heating element
  *  **************************************************************************************************
  ---------------------------------------------------------------------------------------------------*/
 
@@ -86,12 +86,20 @@ const int SCL_PIN = 21;
 // MEGA's outputs
 const int SSR_PULSE_PIN = 2;
 
-// PID constants
+// PID constants and object
 const double TEMP_SETPOINT = 160; // °C
 const double TEMP_RANGE =  0.5; // °C
 const double KP = 1; // proportional's coefficient
 const double KI = 0; // integral's coefficient 
 const double KD = 0; // derivative's coefficient
+PID temp_PID = PID( SSR_PULSE_PIN, TEMP_SETPOINT, TEMP_RANGE, KP, KI, KD );
+
+// PID variable
+double temp_correction; // ratio
+
+// Temperature constant and object
+float TEMP_ERROR = 8; // °C , rectification of the temperature because of the physical montage. Error determined with experimental test and comparison.
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
 // Temperature variables
 float ambiantTemp; // °C
@@ -132,11 +140,14 @@ const float QUIESCENT_OUTPUT_VOLTAGE[] ={
           }; // coefficient
 const float MY_CURRENT_SENSOR_SENSITIVITY = CURRENT_SENSOR_SENSITIVITY[CURRENT_SENSOR_MODEL]/1000; // mV/A · (1V / 1000 mV) → V/A
 const float MY_QOV = QUIESCENT_OUTPUT_VOLTAGE[CURRENT_SENSOR_MODEL] * VCC; // Volts
+const float MAKE_ZERO_V = 0.007; // Volts, to make 0V if there is no current see line where this const is use
 
 // Current variables
-float cutOffLimit = 1.00; // Ampere
+float cutOffLimit = 1.00; // Amps
 float cutOff = MY_CURRENT_SENSOR_SENSITIVITY * cutOffLimit; // V/A · A → Volts
+float voltage_read; // Volts
 float voltage; // Volts
+float current; // Amps
 
 // Revolution constants (ref [4])
 const uint8_t VCNL4000_ADDRESS = 0x13;
@@ -156,6 +167,12 @@ const uint8_t VCNL4000_MEASUREAMBIENT = 0x10;
 const uint8_t VCNL4000_MEASUREPROXIMITY = 0x08;
 const uint8_t VCNL4000_AMBIENTREADY = 0x40;
 const uint8_t VCNL4000_PROXIMITYREADY = 0x20;
+const int MIN_PROXIMITY = 2100; // unknown unit
+
+// Revolution variable
+int rpm; // revolution per minute
+int revolution; // complete rotation
+unsigned int proximity; // unknown unit
 
 // Conversion factor
 const float ANALOG_SCALE_TO_V = (5.0 / 1023.0); // coefficient (Ref [5])
@@ -164,14 +181,24 @@ const float ANALOG_SCALE_TO_V = (5.0 / 1023.0); // coefficient (Ref [5])
 const int SERIAL_SPEED = 9600; // bps → bits per second
 
 
+
 // Configuration of the system
 void setup() {
 
-// Open serial port & set his data rate
-Serial.begin(SERIAL_SPEED);
+   // Open serial port & set his data rate
+  Serial.begin(SERIAL_SPEED);
+  
+  // "Initiate the Wire library and join the I2C bus as a master" (Ref [6])
+  Wire.begin();
+  
+  // Affect the mode of each use pins
+  pinMode( VIOUT_CURRENT_SENSOR_PIN, INPUT );
+  pinMode( SDA_PIN, INPUT ); // Not sure about this line
+  pinMode( SCL_PIN, INPUT ); // Not sure about this line
+  pinMode( SSR_PULSE_PIN, OUTPUT );
 
-// 
-
+  // Enable reading from sensors
+  mlx.begin(); 
 }
 
 
@@ -179,12 +206,71 @@ Serial.begin(SERIAL_SPEED);
 // Continuous communications of the system
 void loop() {
 
+  // Read the temperature
+  headTemp = TEMP_ERROR + mlx.readObjectTempC();
+  ambiantTemp = mlx.readAmbientTempC();
+  
+  // Find the correction to apply in fonction of PWM pins's duty cycle → between 0 & 255 (ref [7])
+  temp_correction = temp_PID.run(headTemp);
+  
+  // Apply correction for the heating element
+  analogWrite( SSR_PULSE_PIN, temp_correction );
+
+  // Read the voltage
+  voltage_read = ANALOG_SCALE_TO_V * analogRead(VIOUT_CURRENT_SENSOR_PIN);
+
+  // Voltage in fonction of quiescent output voltage
+  voltage = voltage_read - MY_QOV + MAKE_ZERO_V;
+
+  // Current in fonction of the voltage(QOV) and the sensitivity
+  current = voltage / MY_CURRENT_SENSOR_SENSITIVITY;
+
+  // Read the proximity
+  proximity = read16(VCNL4000_PROXIMITYDATA);
+/*
+  // Count a revolution
+  if ( MIN_PROXIMITY < proximity ) {
+    revolution += 1;
+  }
+*/ // brainstorm, to be elaborate
+  
+  // Display of all data in the serial monitor (table)
+  
 }
 
 
 
 // Fonctions used in setup() and loop()
 
+// Read 16 bits (2 bytes) of data from the proximity sensor (VCNL4000) at a specified addresse (Ref [4])
+uint16_t read16(uint8_t address)
+{
+  uint16_t data;
+ 
+  Wire.beginTransmission(VCNL4000_ADDRESS);
+#if ARDUINO >= 100
+  Wire.write(address);
+#else
+  Wire.send(address);
+#endif
+  Wire.endTransmission();
+ 
+  Wire.requestFrom(VCNL4000_ADDRESS, 2);
+  while(!Wire.available());
+#if ARDUINO >= 100
+  data = Wire.read();
+  data <<= 8;
+  while(!Wire.available());
+  data |= Wire.read();
+#else
+  data = Wire.receive();
+  data <<= 8;
+  while(!Wire.available());
+  data |= Wire.receive();
+#endif
+ 
+  return data;
+}
 
 
 /*---------------------------------------------------------------------------------------------------
@@ -195,4 +281,6 @@ void loop() {
  * [4] VCNL4000 Entfernungssensor, Helligkeitssensor am Arduino. (2014, décembre 16). https://www.youtube.com/watch?v=Rp5NcXffsBE 
  * ***
  * [5] analogRead()—Arduino Reference. (s. d.). Consulté 13 août 2020, à l’adresse https://www.arduino.cc/reference/en/language/functions/analog-io/analogread/
+ * [6] Arduino—WireBegin. (s. d.). Consulté 14 août 2020, à l’adresse https://www.arduino.cc/en/Reference/WireBegin
+ * [7] analogWrite()—Arduino Reference. (s. d.). Consulté 14 août 2020, à l’adresse https://www.arduino.cc/reference/en/language/functions/analog-io/analogwrite/
 ---------------------------------------------------------------------------------------------------*/
