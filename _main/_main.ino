33cc0fc8150a6b7029a6a5752c5e4164718a0b0a
Valirou <68293328+Valirou@users.noreply.github.com>
2020-08-21 08:19:00 -0400

    Modified current section
    I modified this section because I added a library written by Robojax after he replied to my comment on his Youtube video which I was using as an old reference.

--- _main/_main.ino
+++ _main/_main.ino
@@ -70,9 +70,10 @@
 
 // Outside librairies used in this code ( see https://www.arduino.cc/reference/en/libraries/ )
 
-#include <Adafruit_MLX90614.h> // to read the temperature from the sensor
 #include <Wire.h> // to communicate with I2C / TWI devices ( MEGA → pins : 20 (SDA), 21 (SCL) )
+#include <Adafruit_MLX90614.h> // to read the temperature from the sensor
 #include <PID.h> // to control a PWN pin depending on a reading (proportional–integral–derivative controller)
+#include <Robojax_AllegroACS_Current_Sensor.h> // to read the current from sensor
 
 
 
@@ -86,7 +87,7 @@ const int SCL_PIN = 21;
 // MEGA's outputs
 const int SSR_PULSE_PIN = 2;
 
-// MEGA's external interrupt pin (ref [9])
+// MEGA's external interrupt pin (ref [8])
 const int INTERRUPT_PIN = SDA_PIN; // seem to be a bad idea, how can I associate the address's value?
 
 // PID constants and object
@@ -95,7 +96,7 @@ const double TEMP_RANGE =  0.03125; // %
 const double KP = 1; // proportional's coefficient
 const double KI = 0; // integral's coefficient 
 const double KD = 0; // derivative's coefficient
-PID temp_PID = PID( TEMP_SETPOINT, TEMP_RANGE, KP, KI, KD );
+PID temp_PID( TEMP_SETPOINT, TEMP_RANGE, KP, KI, KD );
 
 // PID variable
 double temp_correction; // ratio
@@ -108,49 +109,47 @@ Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 float ambiantTemp; // °C
 float headTemp; // °C
 
-// Current constants (ref [3])
+// Current constants and object (ref [3])
 const float VCC = 5.0; // Volts
-const int CURRENT_SENSOR_MODEL = 0; // ACS758LCB-050B
+const int CURRENT_SENSOR_MODEL = 3; // ACS758LCB-050B
 /* Reference number used in CURRENT_SENSOR_MODEL in fonction of the sensor's model
-          0 → "ACS758LCB-050B"
-          1 → "ACS758LCB-050U"
-          2 → "ACS758LCB-100B"
-          3 → "ACS758LCB-100U"
-          4 → "ACS758KCB-150B"
-          5 → "ACS758KCB-150U"
-          6 → "ACS758ECB-200B"
-          7 → "ACS758ECB-200U"            
-*/
-const float CURRENT_SENSOR_SENSITIVITY[] ={
-          40.0,// for ACS758LCB-050B
-          60.0,// for ACS758LCB-050U
-          20.0,// for ACS758LCB-100B
-          40.0,// for ACS758LCB-100U
-          13.3,// for ACS758KCB-150B
-          16.7,// for ACS758KCB-150U
-          10.0,// for ACS758ECB-200B
-          20.0,// for ACS758ECB-200U     
-         }; // mV/A
-const float QUIESCENT_OUTPUT_VOLTAGE[] ={
-          0.5,// for ACS758LCB-050B
-          0.12,// for ACS758LCB-050U
-          0.5,// for ACS758LCB-100B
-          0.12,// for ACS758LCB-100U
-          0.5,// for ACS758KCB-150B
-          0.12,// for ACS758KCB-150U
-          0.5,// for ACS758ECB-200B
-          0.12,// for ACS758ECB-200U            
-          }; // coefficient
-const float MY_CURRENT_SENSOR_SENSITIVITY = CURRENT_SENSOR_SENSITIVITY[CURRENT_SENSOR_MODEL]/1000; // mV/A · (1V / 1000 mV) → V/A
-const float MY_QOV = QUIESCENT_OUTPUT_VOLTAGE[CURRENT_SENSOR_MODEL] * VCC; // Volts
-const float MAKE_ZERO_V = 0.007; // Volts, to make 0V if there is no current see line where this const is use
+      0 → ACS712ELCTR-05B
+      1 → ACS712ELCTR-20A
+      2 → ACS712ELCTR-30A
+      
+      3 → ACS758LCB-050B
+      4 → ACS758LCB-050U
+      5 → ACS758LCB-100B
+      6 → ACS758LCB-100U
+      7 → ACS758KCB-150B
+      8 → ACS758KCB-150U
+      9 → ACS758ECB-200B
+      10 → ACS758ECB-200U 
+      
+      11 → ACS770x-050B      
+      12 → ACS770x-050U
+      13 → ACS770x-100B
+      14 → ACS770x-100U
+      15 → ACS770x-150B
+      16 → ACS770x-150U
+      17 → ACS770x-200B  
+      18 → ACS770x-200U  
+      
+      19 → ACS732KLATR-20AB
+      20 → ACS732KLATR-40AB
+      21 → ACS732KLATR-65AB
+      22 → ACS732KLATR-65AU
+      23 → ACS732KLATR-75AB
 
-// Current variables
-float cutOffLimit = 1.00; // Amps
-float cutOff = MY_CURRENT_SENSOR_SENSITIVITY * cutOffLimit; // V/A · A → Volts
-float voltage_read; // Volts
-float voltage; // Volts
-float current; // Amps
+      24 → ACS733KLATR-20AB
+      25 → ACS733KLATR-40AB
+      26 → ACS733KLATR-40AU
+      27 → ACS733KLATR-65AU
+*/
+Robojax_AllegroACS_Current_Sensor ACS758( CURRENT_SENSOR_MODEL,VIOUT_CURRENT_SENSOR_PIN );
+
+// Current variable
+float currentRead;
 
 // Revolution constants (ref [4])
 const uint8_t VCNL4000_ADDRESS = 0x13;
@@ -221,21 +220,10 @@ void loop() {
   // Find the correction to apply in fonction of PWM pins's duty cycle → between 0 & 255 (ref [7])
   temp_correction = temp_PID.PWM_run(SSR_PULSE_PIN, headTemp);
 
-  // Read the voltage
-  voltage_read = ANALOG_SCALE_TO_V * analogRead(VIOUT_CURRENT_SENSOR_PIN);
+  // Read the current
+  currentRead = ACS758.getCurrent();
 
-  // Voltage in fonction of quiescent output voltage
-  voltage = voltage_read - MY_QOV + MAKE_ZERO_V;
-
-  // Current in fonction of the voltage(QOV) and the sensitivity
-  current = voltage / MY_CURRENT_SENSOR_SENSITIVITY;
-
-/*
-  // Read the proximity
-  proximity = read16(VCNL4000_PROXIMITYDATA);
-*/
-
-  // (Ref [8])
+  // (Ref [4])
   // Turn off the interrupt to let the revolution number how it is while calculating RPM
   detachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN));
   
@@ -307,14 +295,11 @@ void isr_RPM() {
  * Code's references:
  * [1] AC 220V Heater Temperature PID and TRIAC control. (2018, avril 8). https://www.youtube.com/watch?v=P6mbBJDIvxI
  * [2] Build An Infrared Thermometer. (2020, mars 28). https://www.youtube.com/watch?v=UID87M-IKsg
- * [3] Measure current with ACS758 Current Sensor and LCD1602-I2C with Arduino. (2018, juin 20). https://www.youtube.com/watch?v=tug9wjCwDQA
- * [4] VCNL4000 Entfernungssensor, Helligkeitssensor am Arduino. (2014, décembre 16). https://www.youtube.com/watch?v=Rp5NcXffsBE 
+ * [3] Measuring 5A to 30A AC and DC current using ACS712 with Robojax Library—Robojax. (s. d.). Consulté 21 août 2020, à l’adresse https://robojax.com/learn/arduino/?vid=robojax_Alegro_ACS712_curren_sensor
+ * [4] Arduino Tutorial : Tachometer (RPM Counter). (s. d.). Consulté 20 août 2020, à l’adresse https://www.youtube.com/watch?v=u2uJMJWsfsg&t=144s
  * ***
  * [5] analogRead()—Arduino Reference. (s. d.). Consulté 13 août 2020, à l’adresse https://www.arduino.cc/reference/en/language/functions/analog-io/analogread/
  * [6] Arduino—WireBegin. (s. d.). Consulté 14 août 2020, à l’adresse https://www.arduino.cc/en/Reference/WireBegin
  * [7] analogWrite()—Arduino Reference. (s. d.). Consulté 14 août 2020, à l’adresse https://www.arduino.cc/reference/en/language/functions/analog-io/analogwrite/
- * ***
- * [8] Easy Peasy Tachometer. (s. d.). Arduino Project Hub. Consulté 28 juillet 2020, à l’adresse https://create.arduino.cc/projecthub/PracticeMakesBetter/easy-peasy-tachometer-20e73a
- * ***
- * [9] Thangavel, Pramoth. (2019, février 12). Arduino Interrupts Tutorial. Circuit Digest. https://circuitdigest.com/microcontroller-projects/arduino-interrupt-tutorial-with-examples
+ * [8] Thangavel, Pramoth. (2019, février 12). Arduino Interrupts Tutorial. Circuit Digest. https://circuitdigest.com/microcontroller-projects/arduino-interrupt-tutorial-with-examples
  ---------------------------------------------------------------------------------------------------*/
--- readCurrent/readCurrent.ino
+++ readCurrent/readCurrent.ino
@@ -1,133 +1,125 @@
-// Program from Robojax, Measure Current using Allegro ACS758 Current Sensor with LCD1602 (I2C) for Arduino, https://www.youtube.com/watch?v=tug9wjCwDQA
-
+// Program from Measuring 5A to 30A AC and DC current using ACS712 with Robojax Library—Robojax, https://robojax.com/learn/arduino/?vid=robojax_Alegro_ACS712_curren_sensor
+ /*
  * 
- * Arduino Sketch for Allegro ACS758 Current Sensor with LCD1602 & I2C module
- * this sensor can measure current at range of up to 200A
- * It operates with 3.3 or 5V
- * This video requires you to watch the following 2 videos before using this code:
- * 1- ACS758 Sensor https://www.youtube.com/watch?v=SiHfjzcqnU4
- * 2- LCD1602 with I2C https://www.youtube.com/watch?v=q9YC_GVHy5A
+ * Arduino Sketch for Allegro ACS712 Current Sensor 
+ * this sensor is offered at 5A, 20A and 30A range for AC and DC
+ 
+ * It operates with 5V
+ * Please watch video instruction and explanation for this code
+  Watch video instruction for ACS770 50A to 200A Sensors https://youtu.be/sB6EULTix2k
+  Watch video instruction for ACS712 5A to 30A Sensors :https://youtu.be/3C33DpcSwIw
+ 
+ 
+ACS712 Video Timing
+00:51 Introduction
+01:31 Hardware Explained
+02:20 Datasheet Viewed
+05:55 Wiring Explained
+      Get wiring diagram from my Arduino Course
+07:36 Code settings explained (library download shown)
+      Full explanation of code at my Arduino Course
+12:28 5A module demonstration
+14:35 30A module demonstration
+17:07 AC current measurement
+19:37 Conclusion remarks
  * 
- * 
- * Written by Ahmad Shamshiri on Tuesday June 19,2018 at 20:40 at Ajax, Ontario, Canada
+ * Written by Ahmad Shamshiri on Saturday Mar 14, 2020 at 16:15 in Ajax, Ontario, Canada
  * for Robojax.com
- * View the video instruction fro this code at https://youtu.be/tug9wjCwDQA
+ * View the video :
  * This code has been downloaded from Robojax.com
+ * 
+model select
+      //ACS712
+      0-"ACS712ELCTR-05B",
+      1-"ACS712ELCTR-20A",
+      2-"ACS712ELCTR-30A",
+      
+      //ACS758
+      3,// for ACS758LCB-050B
+      4,// for ACS758LCB-050U
+      5,// for ACS758LCB-100B
+      6,// for ACS758LCB-100U
+      7,// for ACS758KCB-150B
+      8,// for ACS758KCB-150U
+      9,// for ACS758ECB-200B
+      10,// for ACS758ECB-200U 
+      
+      ///ACS770
+      11,// for ACS770x-050B ///      
+      12,// for ACS770x-050U  
+      13,// for ACS770x-100B
+      14,// for ACS770x-100U
+      15,// for ACS770x-150B
+      16,// for ACS770x-150U
+      17,// for ACS770x-200B  
+      18,// for ACS770x-200U  
+      
+
+      19 for "ACS732KLATR-20AB",
+      20 for "ACS732KLATR-40AB",
+      21 for "ACS732KLATR-65AB", 
+      22 for "ACS732KLATR-65AU",   
+      23 for "ACS732KLATR-75AB", 
+
+      24 for "ACS733KLATR-20AB",
+      25 for "ACS733KLATR-40AB",
+      26 for "ACS733KLATR-40AU", 
+      27 for "ACS733KLATR-65AU",
+
+ * this code has been downloaded from http://robojax.com/learn/arduino/
+ 
+ * Get this code and other Arduino codes from Robojax.com
+Learn Arduino step by step in structured course with all material, wiring diagram and library
+all in once place. Purchase My course on Udemy.com http://robojax.com/L/?id=62
+
+****************************
+Get early access to my videos via Patreon and have  your name mentioned at end of very 
+videos I publish on YouTube here: http://robojax.com/L/?id=63 (watch until end of this video to list of my Patrons)
+****************************
+
+or make donation using PayPal http://robojax.com/L/?id=64
+
+ *  * This code is "AS IS" without warranty or liability. Free to be used as long as you keep this note intact.* 
+ * This code has been download from Robojax.com
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
  */
-#define VIN A0 // define the Arduino pin A0 as voltage input (V in)
-const float VCC   = 5.0;// supply voltage 5V or 3.3V. If using PCB, set to 5V only.
-const int model = 2;   // enter the model (see below)
+const int VIN = A0; // define the Arduino pin A0 as voltage input (V in)
+const float VCC   = 5.04;// supply voltage
+const int MODEL = 0;   // enter the model (see above list)
+#include <Robojax_AllegroACS_Current_Sensor.h>
 
-float cutOffLimit = 1.00;// reading cutt off current. 1.00 is 1 Amper
+Robojax_AllegroACS_Current_Sensor robojax(MODEL,VIN);
 
-/*
-          "ACS758LCB-050B",// for model use 0
-          "ACS758LCB-050U",// for model use 1
-          "ACS758LCB-100B",// for model use 2
-          "ACS758LCB-100U",// for model use 3
-          "ACS758KCB-150B",// for model use 4
-          "ACS758KCB-150U",// for model use 5
-          "ACS758ECB-200B",// for model use 6
-          "ACS758ECB-200U"// for model use  7   
-sensitivity array is holding the sensitivy of the  ACS758
-current sensors. Do not change.          
-*/
-float sensitivity[] ={
-          40.0,// for ACS758LCB-050B
-          60.0,// for ACS758LCB-050U
-          20.0,// for ACS758LCB-100B
-          40.0,// for ACS758LCB-100U
-          13.3,// for ACS758KCB-150B
-          16.7,// for ACS758KCB-150U
-          10.0,// for ACS758ECB-200B
-          20.0,// for ACS758ECB-200U     
-         }; 
-
-/*         
- *   quiescent Output voltage is factor for VCC that appears at output       
- *   when the current is zero. 
- *   for Bidirectional sensor it is 0.5 x VCC
- *   for Unidirectional sensor it is 0.12 x VCC
- *   for model ACS758LCB-050B, the B at the end represents Bidirectional (polarity doesn't matter)
- *   for model ACS758LCB-100U, the U at the end represents Unidirectional (polarity must match)
- *    Do not change.
- */
-float quiescent_Output_voltage [] ={
-          0.5,// for ACS758LCB-050B
-          0.12,// for ACS758LCB-050U
-          0.5,// for ACS758LCB-100B
-          0.12,// for ACS758LCB-100U
-          0.5,// for ACS758KCB-150B
-          0.12,// for ACS758KCB-150U
-          0.5,// for ACS758ECB-200B
-          0.12,// for ACS758ECB-200U            
-          };
-const float FACTOR = sensitivity[model]/1000;// set sensitivity for selected model
-const float QOV =   quiescent_Output_voltage [model] * VCC;// set quiescent Output voltage for selected model
-float voltage;// internal variable for voltage
-float cutOff = FACTOR/cutOffLimit;// convert current cut off to mV
-
-// ======== start of LCD1602 with i2C settings
-#include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
-
-// Set the LCD address to 0x27 for a 16 chars and 2 line display
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-// ======= END of  LCD1602 with i2C settings
 void setup() {
-    //Robojax.com ACS758 Current Sensor 
+    //Robojax.com Allegro ACS Current Sensor 
     Serial.begin(9600);// initialize serial monitor
     Serial.println("Robojax Tutorial");
-    Serial.println("ACS758 Current Sensor");
-    Serial.println("with LCD1602 & I2C");    
-    // initialize the LCD, 
-    lcd.begin();   
-  // Turn on the blacklight and print a message.
-  lcd.backlight(); 
-    lcd.clear();
-  lcd.print("Robojax");
-  lcd.setCursor (0,1); // go to start of 2nd line 
-  lcd.print("ACS758 Current Sensor"); 
-  delay(2000); 
-  lcd.clear();  
+    Serial.println("ACS770 Current Sensor");
+ 
 }
 
 void loop() {
-  //Robojax code ACS758 with LCD1602 and I2C
-  float voltage_raw =   (5.0 / 1023.0)* analogRead(VIN);// Read the voltage from sensor
-  voltage =  voltage_raw - QOV + 0.007 ;// 0.007 is a value to make voltage zero when there is no current
-  float current = voltage / FACTOR;
-  if(abs(voltage) > cutOff ){
-    Serial.print("V: ");
-    Serial.print(voltage,3);// print voltage with 3 decimal places
-    Serial.print("V, I: ");
-    Serial.print(current,2); // print the current with 2 decimal places
+   //Robojax.com Allegro ACS Current Sensor  
+
+    Serial.print("Current: ");
+    Serial.print(robojax.getCurrent(),3); // print the current with 3 decimal places
+    Serial.print("A Avg:");
+    Serial.print(robojax.getCurrentAverage(300),3);//   
     Serial.println("A");
-  //start of loop Robojax code ACS758 with LCD1602 and I2C
-  lcd.clear();
-  lcd.setCursor (0,0); // set to line 1, char 0  
-  lcd.print("Current: ");
-  lcd.setCursor (9,0); // go to start of 2nd line
-  lcd.print(current);
-  lcd.setCursor (15,0); // go to start of 2nd line
-  lcd.print("A");
-
-  lcd.setCursor (0,1);    
-  lcd.print("Sense V: ");
-  lcd.setCursor (9,1); // go to start of 2nd line
-  lcd.print(voltage);
-  lcd.setCursor (15,1); // go to start of 2nd line
-  lcd.print("V");   
-  lcd.backlight();
- //end of loopcode Robojax code ACS758 with LCD1602 and I2C
-
-
-    
-  }else{
-    Serial.println("No Current");
-  lcd.clear();    
-  lcd.setCursor (0,0);    
-  lcd.print("No Current");    
-  }
-  delay(500);
+   //robojax.debug();
+   
+  delay(1500);
 }
+ 
